9578058b760a82d89d0da6ad1a04d05cab9fb061
david05-Bonilla <bonilla.coca2027@hilasal.superate.org.sv>
2025-07-23 16:04:27 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let mascotas = 7
+
+if (mascotas == 3) {
+    console.log(" tiene pocas mascotas");
+} else if (mascotas > 3) {
+    console.log("tiene bastantes mascotas");
+} else {
+    console.log("no tiene mascotas");
+}
---
+++ js/index2.js
@@ -0,0 +1,20 @@
+let Deportes = 1
+switch (Deportes) {
+    case 1:
+        alert("Futbo0l")
+        
+        break;
+
+    case 2 :
+        alert("Basketbool")
+
+        break;
+
+    case 3 :
+        alert("baisebool")
+
+        break
+
+    default:
+        break;
+}
